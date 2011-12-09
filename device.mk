f7632b272767a0c65bdd6eb657dc86e245d6cad7
Jon Medhurst <tixy@linaro.org>
2011-12-09 16:50:07 +0000

    vexpress-a9: Copy dalvik setup from full_panda
    This fixes OutOfMemory errors when wallpapers are loaded.
    
    Change-Id: I9dbcd6adf09a73ab03816c1e7c6d7d0298ac5748
    Signed-off-by: Jon Medhurst <tixy@linaro.org>

--- device.mk
+++ device.mk
@@ -9,3 +9,7 @@ PRODUCT_COPY_FILES += \
     device/linaro/vexpress_a9/initlogo.rle:root/initlogo.rle
 
 PRODUCT_CHARACTERISTICS := tablet,nosdcard
+
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+$(call inherit-product, frameworks/base/build/tablet-dalvik-heap.mk)
