19bb8e918c3d7b19df4baae83503186bed67c00c
Jon Medhurst <tixy@linaro.org>
2012-02-28 16:39:09 +0000

    vexpress: init.rc tidyups
    Stop using the deprecated init.rc from device/linaro/common and use
    the one in system/core/rootdir instead.
    
    Also, remove redundant gator startup entries in the vexpress specific
    init file, as the generic init.rc handles this.
    
    Change-Id: I0d75fdf1208041226e70c19d7fd420728e8c83eb
    Signed-off-by: Jon Medhurst <tixy@linaro.org>

--- device.mk
+++ device.mk
@@ -3,7 +3,7 @@ PRODUCT_COPY_FILES := \
     device/linaro/vexpress_a9/egl.cfg:system/lib/egl/egl.cfg
 
 PRODUCT_COPY_FILES += \
-    device/linaro/common/init.rc:root/init.rc \
+    system/core/rootdir/init.rc:root/init.rc \
     device/linaro/vexpress_a9/init.arm-versatileexpress.rc:root/init.arm-versatileexpress.rc \
     device/linaro/vexpress_a9/init.vexpress_a9.sh:system/etc/init.vexpress_a9.sh \
     device/linaro/vexpress_a9/initlogo.rle:root/initlogo.rle
