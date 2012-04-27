02761eab440db0869b373b1dea189b26eab53cae
Jon Medhurst <tixy@linaro.org>
2012-04-27 09:56:44 +0100

    vexpress: Start building UEFI ROMs for the A5 and A15 CoreTiles
    Change-Id: I2ccb39da0c7a7758a916a60754bb1818f2df9524
    Signed-off-by: Jon Medhurst <tixy@linaro.org>

--- uefi.mk
+++ uefi.mk
@@ -1,1 +1,3 @@
 $(eval $(call MAKE_EDK2_ROM,ArmPlatformPkg/ArmVExpressPkg/ArmVExpress-CTA9x4.dsc -D EDK2_ARMVE_SINGLE_BINARY=1 -D EDK2_ARMVE_STANDALONE=1,ca9,ARMVEXPRESS_EFI,uefi_v2p-ca9.bin))
+$(eval $(call MAKE_EDK2_ROM,ArmPlatformPkg/ArmVExpressPkg/ArmVExpress-CTA15x2.dsc -D EDK2_ARMVE_SINGLE_BINARY=1 -D EDK2_ARMVE_STANDALONE=1,ca15-tc1,ARMVEXPRESS_EFI,uefi_v2p-ca15-tc1.bin))
+$(eval $(call MAKE_EDK2_ROM,ArmPlatformPkg/ArmVExpressPkg/ArmVExpress-CTA5s.dsc -D EDK2_ARMVE_SINGLE_BINARY=1 -D EDK2_ARMVE_STANDALONE=1,ca5s,ARMVEXPRESS_EFI,uefi_v2p-ca5s.bin))
