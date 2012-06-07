938eb1d9f58ed8d2d6f17d58ea95783c3984c6ad
Jon Medhurst <tixy@linaro.org>
2012-06-07 16:27:12 +0100

    vexpress: Add camera HAL
    Add Linaro's generic camera HAL to vexpress builds and tidyup
    handling of /dev/video* entries.
    
    This does not complete enablement of camera on vexpress: the kernel
    config needs USB Video Class enabling and even with this video
    capture fails with EPIPE.
    
    Change-Id: If1fb6b73d9fcb06b15fa8844691a1c0ef959ebe3
    Signed-off-by: Jon Medhurst <tixy@linaro.org>

--- device.mk
+++ device.mk
@@ -5,11 +5,15 @@ PRODUCT_COPY_FILES := \
 
 PRODUCT_COPY_FILES += \
     device/linaro/vexpress/init.arm-versatileexpress.rc:root/init.arm-versatileexpress.rc \
+    device/linaro/vexpress/ueventd.arm-versatileexpress.rc:root/ueventd.arm-versatileexpress.rc \
     device/linaro/vexpress/init.vexpress.sh:system/etc/init.vexpress.sh \
     device/linaro/vexpress/initlogo.rle:root/initlogo.rle
 
 PRODUCT_CHARACTERISTICS := tablet,nosdcard
 
 PRODUCT_TAGS += dalvik.gc.type-precise
+
+PRODUCT_PACKAGES += \
+ 	camera.vexpress
 
 $(call inherit-product, frameworks/base/build/tablet-dalvik-heap.mk)
