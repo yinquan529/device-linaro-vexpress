dcad080918750fa9e80c9549e768455af0339619
Jon Medhurst <tixy@linaro.org>
2012-09-06 11:53:47 +0100

    vexpress: Build RTSM semi-hosting boot-wrapper
    With this bootwrapper and the device-trees we already build
    we now have all the components in place to boot a vexpress
    build on ARM's Fast Models.
    
    Change-Id: I5892e464987955c1d8f6838b40b7d7d93fcc6381
    Signed-off-by: Jon Medhurst <tixy@linaro.org>

--- bootloader.mk
+++ bootloader.mk
@@ -1,3 +1,14 @@
 $(eval $(call MAKE_EDK2_ROM,ArmPlatformPkg/ArmVExpressPkg/ArmVExpress-CTA9x4.dsc -D EDK2_ARMVE_SINGLE_BINARY=1 -D EDK2_ARMVE_STANDALONE=1,ca9,ARMVEXPRESS_EFI,uefi_v2p-ca9.bin))
 $(eval $(call MAKE_EDK2_ROM,ArmPlatformPkg/ArmVExpressPkg/ArmVExpress-CTA15x2.dsc -D EDK2_ARMVE_SINGLE_BINARY=1 -D EDK2_ARMVE_STANDALONE=1,ca15-tc1,ARMVEXPRESS_EFI,uefi_v2p-ca15-tc1.bin))
 $(eval $(call MAKE_EDK2_ROM,ArmPlatformPkg/ArmVExpressPkg/ArmVExpress-CTA5s.dsc -D EDK2_ARMVE_SINGLE_BINARY=1 -D EDK2_ARMVE_STANDALONE=1,ca5s,ARMVEXPRESS_EFI,uefi_v2p-ca5s.bin))
+
+
+#
+# RTSM semi-hosting boot-wrapper
+#
+$(BOOTLOADER_OUT)/rtsm/linux-system-semi.axf : $(ACP) FORCE_BOOTLOADER_REMAKE
+	cd $(TOP)/boot-wrapper && $(MAKE_RTSM_BOOTWRAPPER) linux-system-semi.axf
+	@mkdir -p $(dir $@)
+	$(ACP) -fpt $(TOP)/boot-wrapper/linux-system-semi.axf $@
+
+BOOTLOADER_TARGETS += $(BOOTLOADER_OUT)/rtsm/linux-system-semi.axf
