4076a94d112eb8ada91a367bac0a73110c97b38d
Jon Medhurst <tixy@linaro.org>
2012-10-15 12:50:20 +0100

    vexpress: Start building UEFI for TC2
    Latest UEFI code now has working support for TC2.
    
    Change-Id: Iba94e5945d86b26397054cfd0b4afe7ec5dec73c
    Signed-off-by: Jon Medhurst <tixy@linaro.org>

--- bootloader.mk
+++ bootloader.mk
@@ -1,6 +1,7 @@
 $(eval $(call MAKE_EDK2_ROM,ArmPlatformPkg/ArmVExpressPkg/ArmVExpress-CTA9x4.dsc -D EDK2_ARMVE_SINGLE_BINARY=1 -D EDK2_ARMVE_STANDALONE=1,ca9,ARMVEXPRESS_EFI,uefi_v2p-ca9.bin))
 $(eval $(call MAKE_EDK2_ROM,ArmPlatformPkg/ArmVExpressPkg/ArmVExpress-CTA15x2.dsc -D EDK2_ARMVE_SINGLE_BINARY=1 -D EDK2_ARMVE_STANDALONE=1,ca15-tc1,ARMVEXPRESS_EFI,uefi_v2p-ca15-tc1.bin))
 $(eval $(call MAKE_EDK2_ROM,ArmPlatformPkg/ArmVExpressPkg/ArmVExpress-CTA5s.dsc -D EDK2_ARMVE_SINGLE_BINARY=1 -D EDK2_ARMVE_STANDALONE=1,ca5s,ARMVEXPRESS_EFI,uefi_v2p-ca5s.bin))
+$(eval $(call MAKE_EDK2_ROM,ArmPlatformPkg/ArmVExpressPkg/ArmVExpress-CTA15-A7.dsc -D ARM_BIGLITTLE_TC2=1 -D EDK2_ARMVE_SINGLE_BINARY=1,ca15-tc2,ARM_VEXPRESS_CTA15A7_EFI,uefi_v2p-ca15-tc2.bin))
 
 
 #
