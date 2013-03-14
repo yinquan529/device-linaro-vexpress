80d7df998d5fd404100e95f91787c5a1020a09e4
Jon Medhurst <tixy@linaro.org>
2013-03-14 10:24:17 +0000

    vexpress: Add support for using filesystems on USB
    Add a new hardware variant "arm-versatileexpress-usb" which
    will mount filesystems from a USB drive rather than MMC.
    
    To select this variant, add the following to the kernel commanline:
      androidboot.hardware=arm-versatileexpress-usb
    
    Note, when booting the USB variant, the 'sdcard' partition (partition 6)
    on the MMC card will still be used as the external storage, if it is
    present. It it is not present then the partition on USB will be used.
    
    Change-Id: Iff0cbcabd0cc39d6f0cae5fc2ee604cb3c98ef5b
    Signed-off-by: Jon Medhurst <tixy@linaro.org>

--- device.mk
+++ device.mk
@@ -1,12 +1,17 @@
 PRODUCT_COPY_FILES := \
     device/linaro/common/fstab.partitions:root/fstab.arm-versatileexpress \
+    device/linaro/vexpress/fstab.arm-versatileexpress-usb:root/fstab.arm-versatileexpress-usb \
     device/linaro/common/fstab.partitions:root/fstab.v2p-aarch64 \
     device/linaro/vexpress/vold.fstab:system/etc/vold.fstab \
     device/linaro/vexpress/egl.cfg:system/lib/egl/egl.cfg
 
 PRODUCT_COPY_FILES += \
     device/linaro/vexpress/init.arm-versatileexpress.rc:root/init.arm-versatileexpress.rc \
+    device/linaro/vexpress/init.partitions.arm-versatileexpress.rc:root/init.partitions.arm-versatileexpress.rc \
     device/linaro/vexpress/ueventd.arm-versatileexpress.rc:root/ueventd.arm-versatileexpress.rc \
+    device/linaro/vexpress/init.arm-versatileexpress.rc:root/init.arm-versatileexpress-usb.rc \
+    device/linaro/vexpress/init.partitions.arm-versatileexpress-usb.rc:root/init.partitions.arm-versatileexpress-usb.rc \
+    device/linaro/vexpress/ueventd.arm-versatileexpress.rc:root/ueventd.arm-versatileexpress-usb.rc \
     device/linaro/vexpress/init.v2p-aarch64.rc:root/init.v2p-aarch64.rc \
     device/linaro/vexpress/ueventd.v2p-aarch64.rc:root/ueventd.v2p-aarch64.rc \
     device/linaro/vexpress/init.vexpress.sh:system/etc/init.vexpress.sh \
