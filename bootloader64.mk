9e4f48e5f699bdef4f805a1a87eb0a14672414c2
Axel Fagerstedt <axel.fagerstedt@linaro.org>
2013-04-18 10:36:32 +0000

    Add symlink required for boot-wrapper to build
    vexpress-v2p-aarch64.dts now includes rtsm_ve-motherboard.dtsi
    so it needs to be symlinked so the include can be resolved.
    
    Also adding rtsm_ve-aemv8a.dts in preperation for moving to
    the correct upstream name.
    
    Change-Id: I0a5579d5814e3b176d591fc37ffa22710eb19559

--- bootloader64.mk
+++ bootloader64.mk
@@ -5,6 +5,8 @@ $(BOOTLOADER_OUT)/linux-system.axf: $(ACP) $(INSTALLED_KERNEL_TARGET) $(INSTALLE
 	ln -sf $(abspath $(TOP)/kernel/arch/arm64/boot/dts/vexpress-v2p-aarch64.dts) $(TOP)/boot-wrapper/vexpress-v2p-aarch64.dts
 	ln -sf $(abspath $(TOP)/kernel/arch/arm64/boot/dts/vexpress-v2m-rs1.dtsi) $(TOP)/boot-wrapper/vexpress-v2m-rs1.dtsi
 	ln -sf $(abspath $(TOP)/kernel/arch/arm64/boot/dts/skeleton.dtsi) $(TOP)/boot-wrapper/skeleton.dtsi
+	ln -sf $(abspath $(TOP)/kernel/arch/arm64/boot/dts/rtsm_ve-motherboard.dtsi) $(TOP)/boot-wrapper/rtsm_ve-motherboard.dtsi
+	ln -sf $(abspath $(TOP)/kernel/arch/arm64/boot/dts/rtsm_ve-aemv8a.dts) $(TOP)/boot-wrapper/rtsm_ve-aemv8a.dts
 	ln -sf $(abspath $(INSTALLED_KERNEL_TARGET)) $(TOP)/boot-wrapper/Image
 	ln -sf $(abspath $(INSTALLED_RAMDISK_TARGET)) $(TOP)/boot-wrapper/filesystem.cpio.gz
 	PATH=$(abspath $(TOP)/gcc-linaro-aarch64-linux-gnu-4.7/bin):$(PATH) && \
