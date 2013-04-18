61d092a9dcee038833a9915a82998a8d8fa3eb73
Vishal Bhoj <vishal.bhoj@linaro.org>
2013-04-18 09:49:35 +0000

    vexpress: iks:  Use upstream TC2 DTS name similar to mp build
    Change-Id: I6cd09c5728f3b3d8c007e66effcf3f984048c1a9
    Signed-off-by: Vishal Bhoj <vishal.bhoj@linaro.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,7 +53,7 @@ else ifeq ($(strip $(BUILD_IKS)),true)
 ifeq ($(KERNEL_CONFIG),)
 KERNEL_CONFIG := vexpress_bL_defconfig
 endif
-DEVICE_TREES := vexpress-v2p-ca15-tc2:v2p-ca15-tc2.dtb
+DEVICE_TREES := vexpress-v2p-ca15_a7:v2p-ca15-tc2.dtb
 UBOOT_FLAVOURS := vexpress_ca5x2:u-boot.bin
 CUSTOM_BOOTLOADER_MAKEFILE := device/linaro/vexpress/bootloader-uboot-files.mk
 INSTALLED_KERNEL_TARGET_NAME := zImage
