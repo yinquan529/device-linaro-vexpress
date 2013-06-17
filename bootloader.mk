9b0e23f1d777ac10c416bb6cb2083e3e3b90448e
Jon Medhurst <tixy@linaro.org>
2013-06-17 15:09:55 +0100

    vexpress: Add UEFI build for A9 and A15 models
    Change-Id: I11d4f58da5c6fcb5a1a790b7da01704b8cff1da2
    Signed-off-by: Jon Medhurst <tixy@linaro.org>

--- bootloader.mk
+++ bootloader.mk
@@ -2,6 +2,8 @@ $(eval $(call MAKE_EDK2_ROM,ArmPlatformPkg/ArmVExpressPkg/ArmVExpress-CTA9x4.dsc
 $(eval $(call MAKE_EDK2_ROM,ArmPlatformPkg/ArmVExpressPkg/ArmVExpress-CTA15x2.dsc -D EDK2_ARMVE_SINGLE_BINARY=1 -D EDK2_ARMVE_STANDALONE=1,ca15-tc1,ARMVEXPRESS_EFI,uefi_v2p-ca15-tc1.bin))
 $(eval $(call MAKE_EDK2_ROM,ArmPlatformPkg/ArmVExpressPkg/ArmVExpress-CTA5s.dsc -D EDK2_ARMVE_SINGLE_BINARY=1 -D EDK2_ARMVE_STANDALONE=1,ca5s,ARMVEXPRESS_EFI,uefi_v2p-ca5s.bin))
 $(eval $(call MAKE_EDK2_ROM,ArmPlatformPkg/ArmVExpressPkg/ArmVExpress-CTA15-A7.dsc -D ARM_BIGLITTLE_TC2=1 -D EDK2_ARMVE_SINGLE_BINARY=1,ca15-tc2,ARM_VEXPRESS_CTA15A7_EFI,uefi_v2p-ca15-tc2.bin))
+$(eval $(call MAKE_EDK2_ROM,ArmPlatformPkg/ArmVExpressPkg/ArmVExpress-RTSM-A9x4.dsc -D EDK2_ARMVE_STANDALONE=1,rtsm-ca9,RTSM_VE_CORTEX-A9_EFI,rtsm/uefi_rtsm_ve-ca9x4.bin))
+$(eval $(call MAKE_EDK2_ROM,ArmPlatformPkg/ArmVExpressPkg/ArmVExpress-RTSM-A15_MPCore.dsc -D EDK2_ARMVE_STANDALONE=1,rtsm-ca15,RTSM_VE_CORTEX-A15_MPCORE_EFI,rtsm/uefi_rtsm_ve-ca15.bin))
 
 
 #
