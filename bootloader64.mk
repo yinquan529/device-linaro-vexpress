5bbed6080e0945a6387efdf9fc90cdca6503d5bd
Amit Pundir <amit.pundir@linaro.org>
2013-09-15 18:25:19 +0530

    bootloader64: update juice kernel path and cleanup
    Update juice kernel path and cleanup post build binaries in
    boot-wrapper directory.
    
    Change-Id: I411e5930c460a14c6beb1dacb537113d6a0894e3
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- bootloader64.mk
+++ bootloader64.mk
@@ -2,11 +2,11 @@ $(INSTALLED_BOOTTARBALL_TARGET): $(BOOTLOADER_OUT)/linux-system.axf
 
 $(BOOTLOADER_OUT)/linux-system.axf: $(ACP) $(INSTALLED_KERNEL_TARGET) $(INSTALLED_RAMDISK_TARGET)
 	ln -sf $(abspath $(KERNEL_OUT)/scripts/dtc/dtc) $(TOP)/boot-wrapper/dtc
-	ln -sf $(abspath $(TOP)/kernel/arch/arm64/boot/dts/vexpress-v2p-aarch64.dts) $(TOP)/boot-wrapper/vexpress-v2p-aarch64.dts
-	ln -sf $(abspath $(TOP)/kernel/arch/arm64/boot/dts/vexpress-v2m-rs1.dtsi) $(TOP)/boot-wrapper/vexpress-v2m-rs1.dtsi
-	ln -sf $(abspath $(TOP)/kernel/arch/arm64/boot/dts/skeleton.dtsi) $(TOP)/boot-wrapper/skeleton.dtsi
-	ln -sf $(abspath $(TOP)/kernel/arch/arm64/boot/dts/rtsm_ve-motherboard.dtsi) $(TOP)/boot-wrapper/rtsm_ve-motherboard.dtsi
-	ln -sf $(abspath $(TOP)/kernel/arch/arm64/boot/dts/rtsm_ve-aemv8a.dts) $(TOP)/boot-wrapper/rtsm_ve-aemv8a.dts
+	ln -sf $(abspath $(TOP)/$(TARGET_KERNEL_SOURCE)/arch/arm64/boot/dts/vexpress-v2p-aarch64.dts) $(TOP)/boot-wrapper/vexpress-v2p-aarch64.dts
+	ln -sf $(abspath $(TOP)/$(TARGET_KERNEL_SOURCE)/arch/arm64/boot/dts/vexpress-v2m-rs1.dtsi) $(TOP)/boot-wrapper/vexpress-v2m-rs1.dtsi
+	ln -sf $(abspath $(TOP)/$(TARGET_KERNEL_SOURCE)/arch/arm64/boot/dts/skeleton.dtsi) $(TOP)/boot-wrapper/skeleton.dtsi
+	ln -sf $(abspath $(TOP)/$(TARGET_KERNEL_SOURCE)/arch/arm64/boot/dts/rtsm_ve-motherboard.dtsi) $(TOP)/boot-wrapper/rtsm_ve-motherboard.dtsi
+	ln -sf $(abspath $(TOP)/$(TARGET_KERNEL_SOURCE)/arch/arm64/boot/dts/rtsm_ve-aemv8a.dts) $(TOP)/boot-wrapper/rtsm_ve-aemv8a.dts
 	ln -sf $(abspath $(INSTALLED_KERNEL_TARGET)) $(TOP)/boot-wrapper/Image
 	ln -sf $(abspath $(INSTALLED_RAMDISK_TARGET)) $(TOP)/boot-wrapper/filesystem.cpio.gz
 	PATH=$(abspath $(TOP)/gcc-linaro-aarch64-linux-gnu/bin):$(PATH) && \
@@ -19,5 +19,8 @@ $(BOOTLOADER_OUT)/linux-system.axf: $(ACP) $(INSTALLED_KERNEL_TARGET) $(INSTALLE
 	rm $(TOP)/boot-wrapper/skeleton.dtsi
 	rm $(TOP)/boot-wrapper/Image
 	rm $(TOP)/boot-wrapper/filesystem.cpio.gz
+	rm $(TOP)/boot-wrapper/fdt.dtb
+	rm $(TOP)/boot-wrapper/rtsm_ve-aemv8a.dts
+	rm $(TOP)/boot-wrapper/rtsm_ve-motherboard.dtsi
 
 BOOTLOADER_TARGETS += $(BOOTLOADER_OUT)/linux-system.axf
