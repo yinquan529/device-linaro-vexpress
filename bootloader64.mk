29a008e8923d77c948b97f1cb76fd82eb714452f
Amit Pundir <amit.pundir@linaro.org>
2013-10-08 12:50:17 +0530

    bootloader64.mk: do not mix fvp-base and axf build targets
    Make fvp-base-gicv2-psci-android.dtb an independent target.
    
    Change-Id: Ib04390268b730be839bf50636230a877db6941f6
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- bootloader64.mk
+++ bootloader64.mk
@@ -1,4 +1,4 @@
-$(INSTALLED_BOOTTARBALL_TARGET): $(BOOTLOADER_OUT)/linux-system.axf
+$(INSTALLED_BOOTTARBALL_TARGET): $(BOOTLOADER_OUT)/linux-system.axf $(BOOTLOADER_OUT)/fvp-base-gicv2-psci-android.dtb
 
 $(BOOTLOADER_OUT)/linux-system.axf: $(ACP) $(INSTALLED_KERNEL_TARGET) $(INSTALLED_RAMDISK_TARGET)
 	ln -sf $(abspath $(KERNEL_OUT)/scripts/dtc/dtc) $(TOP)/boot-wrapper/dtc
@@ -7,16 +7,12 @@ $(BOOTLOADER_OUT)/linux-system.axf: $(ACP) $(INSTALLED_KERNEL_TARGET) $(INSTALLE
 	ln -sf $(abspath $(TOP)/$(TARGET_KERNEL_SOURCE)/arch/arm64/boot/dts/skeleton.dtsi) $(TOP)/boot-wrapper/skeleton.dtsi
 	ln -sf $(abspath $(TOP)/$(TARGET_KERNEL_SOURCE)/arch/arm64/boot/dts/rtsm_ve-motherboard.dtsi) $(TOP)/boot-wrapper/rtsm_ve-motherboard.dtsi
 	ln -sf $(abspath $(TOP)/$(TARGET_KERNEL_SOURCE)/arch/arm64/boot/dts/rtsm_ve-aemv8a.dts) $(TOP)/boot-wrapper/rtsm_ve-aemv8a.dts
-	ln -sf $(abspath $(TOP)/$(TARGET_KERNEL_SOURCE)/arch/arm64/boot/dts/fvp-base-gicv2-psci-android.dts) $(TOP)/boot-wrapper/fvp-base-gicv2-psci-android.dts
 	ln -sf $(abspath $(INSTALLED_KERNEL_TARGET)) $(TOP)/boot-wrapper/Image
 	ln -sf $(abspath $(INSTALLED_RAMDISK_TARGET)) $(TOP)/boot-wrapper/filesystem.cpio.gz
 	PATH=$(abspath $(TOP)/gcc-linaro-aarch64-linux-gnu/bin):$(PATH) && \
 	$(MAKE) -C $(TOP)/boot-wrapper CROSS_COMPILE=$(KERNEL_TOOLS_PREFIX)
 	@mkdir -p $(dir $@)
 	$(ACP) -fpt $(TOP)/boot-wrapper/linux-system.axf $@
-	$(ACP) -fpt $(TOP)/boot-wrapper/fvp-base-gicv2-psci-android.dtb $(BOOTLOADER_OUT)
-	rm $(TOP)/boot-wrapper/fvp-base-gicv2-psci-android.dtb
-	rm $(TOP)/boot-wrapper/fvp-base-gicv2-psci-android.dts
 	rm $(TOP)/boot-wrapper/dtc
 	rm $(TOP)/boot-wrapper/vexpress-v2p-aarch64.dts
 	rm $(TOP)/boot-wrapper/vexpress-v2m-rs1.dtsi
@@ -26,5 +22,16 @@ $(BOOTLOADER_OUT)/linux-system.axf: $(ACP) $(INSTALLED_KERNEL_TARGET) $(INSTALLE
 	rm $(TOP)/boot-wrapper/fdt.dtb
 	rm $(TOP)/boot-wrapper/rtsm_ve-aemv8a.dts
 	rm $(TOP)/boot-wrapper/rtsm_ve-motherboard.dtsi
+
+$(BOOTLOADER_OUT)/fvp-base-gicv2-psci-android.dtb: $(ACP) $(INSTALLED_KERNEL_TARGET)
+	ln -sf $(abspath $(KERNEL_OUT)/scripts/dtc/dtc) $(TOP)/boot-wrapper/dtc
+	ln -sf $(abspath $(TOP)/$(TARGET_KERNEL_SOURCE)/arch/arm64/boot/dts/fvp-base-gicv2-psci-android.dts) $(TOP)/boot-wrapper/fvp-base-gicv2-psci-android.dts
+	@mkdir -p $(dir $@)
+	$(MAKE) -C $(TOP)/boot-wrapper fvp-base-gicv2-psci-android.dtb
+	@mkdir -p $(dir $@)
+	$(ACP) -fpt $(TOP)/boot-wrapper/fvp-base-gicv2-psci-android.dtb $@
+	rm $(TOP)/boot-wrapper/fvp-base-gicv2-psci-android.dtb
+	rm $(TOP)/boot-wrapper/fvp-base-gicv2-psci-android.dts
+	rm $(TOP)/boot-wrapper/dtc
 
 BOOTLOADER_TARGETS += $(BOOTLOADER_OUT)/linux-system.axf
