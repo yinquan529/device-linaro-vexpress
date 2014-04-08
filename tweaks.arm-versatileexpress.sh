c1ef9c3433013c63f3b9d6894e6950c1748f3dba
Jon Medhurst <tixy@linaro.org>
2014-04-08 10:01:16 +0100

    vexpress: Initialise HMP tuning values at boot on TC2
    These values were being set up by the kernel when it was built for
    TC2, but that doesn't work nicely for multi-platform kernels.
    So lets initialise the values in the Android boot scripts instead,
    which lets the kernel return to having some sensible generic
    defaults.
    
    Change-Id: I60483bae45cba60efe87bcfb733b45f8b8c4b58a
    Signed-off-by: Jon Medhurst <tixy@linaro.org>

--- tweaks.arm-versatileexpress.sh
+++ tweaks.arm-versatileexpress.sh
@@ -5,3 +5,15 @@ then
 	# We're running on a fast model so speed things up by...
 	setprop debug.sf.nobootanimation 1
 fi
+
+if grep -q -e V2P-CA15_CA7 /proc/device-tree/model
+then
+	# We're running on TC2, set some HMP scheduler tunables...
+
+	echo 0 > /sys/kernel/hmp/packing_enable
+
+	# TC2 has a sharp consumption curve @ around 800Mhz, so
+	# we aim to spread the load around that frequency.
+	# '650' is  80% of the 800Mhz freq * NICE_0_LOAD
+	echo 650 > /sys/kernel/hmp/packing_limit
+fi
