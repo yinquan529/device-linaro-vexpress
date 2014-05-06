b878646526047c6060c239f00cf98701a7af0d8f
Jon Medhurst <tixy@linaro.org>
2014-05-06 15:22:31 +0100

    vexpress: Enable HMP task packing on TC2
    Even though task packing doesn't make noticeable difference to TC2
    performance or power, we want it enabled by default to match the
    expected usage on other platforms and to increase test coverage of
    the feature.
    
    Change-Id: Id557af22aed4eda739d20b360e40d7b4adbea976
    Signed-off-by: Jon Medhurst <tixy@linaro.org>

--- tweaks.arm-versatileexpress.sh
+++ tweaks.arm-versatileexpress.sh
@@ -10,7 +10,7 @@ if grep -q -e V2P-CA15_CA7 /proc/device-tree/model
 then
 	# We're running on TC2, set some HMP scheduler tunables...
 
-	echo 0 > /sys/kernel/hmp/packing_enable
+	echo 1 > /sys/kernel/hmp/packing_enable
 
 	# TC2 has a sharp consumption curve @ around 800Mhz, so
 	# we aim to spread the load around that frequency.
